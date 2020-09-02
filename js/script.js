d28cf5801b4d93260de8cd362c78c87546f88ef0
Ayat Elsherif <ayat_elsherif@yahoo.com>
2020-09-02 21:45:32 +0200

    merging the 2 registration forms,edit social media

--- js/script.js
+++ js/script.js
@@ -1,22 +1,8 @@
 /************** the offCanvas - left side bar**************************/
 $(function () {
 
-  // $(".btn-close, .screen-overlay, #navbarSupportedContent .nav-item").click(function (e) {
-  //   $(".screen-overlay").removeClass("showCanvas");
-  //   $(".mobile-offcanvas").removeClass("showCanvas");
-  //   $("body").removeClass("offcanvas-active");
-  // });
-  // $(window).resize(function () {
-  //   if ($(".screen-overlay").hasClass("showCanvas")) {
-  //       $(".mobile-offcanvas").removeClass("showCanvas");
-  //       $("body").removeClass("offcanvas-active");
-  //       $(".screen-overlay").removeClass("showCanvas");
-  //     }
-  // });
-
   $(".offcanvas-header, .setAddress.setAddressLarge:not('#accordion') a").click(function () {
       $('.mobile-offcanvas').toggleClass("showCanvas");
-
   });
 
   $('.setAddressSmall').click(function(e){
@@ -81,7 +67,7 @@ $(".buttonFile").click(function () {
 
 /************************************slide from photos hint to photos uplaod***********************************/
 
-$(".hintPhotos button").click(function () {
+$(".hintPhotos .fileButton a").click(function () {
   $(this).parents(".hintPhotos").animate({
     left: '-100%'
   }).fadeOut('fast', function () {
@@ -93,3 +79,55 @@ $(".hintPhotos button").click(function () {
 
 
 /********************************************************************************** */
+
+
+/******************************Add a property MultiForm***************************** */
+let nextIH=document.getElementsByClassName('nextIH')
+let backIH=document.getElementsByClassName('backIH')
+let createIH=document.getElementsByClassName('createIH')
+let step1=document.getElementsByClassName('step1');
+let step2=document.getElementsByClassName('step2');
+$(nextIH).click(function(e){
+  e.stopPropagation();
+  if($('#validationCustom04').change())
+  {
+  $(step1).hide();
+  $(step2).show();
+  }
+  else return false;
+});
+
+$(backIH).click(function(e){
+  $(step2).hide();
+  $(step1).show();
+});
+
+/***************************End Add a property MultiForm************************** */
+
+
+
+/**************************************open social media from android in mobile view**************************************** */
+
+const shareButton = document.querySelector('.IHShare');
+const shareDialog = document.querySelector('.collapseIHShare');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'IH Address',
+      url: 'https://ihaddress.com',
+      text:'971.34.344.7777'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    }
+    //  else {
+    //     shareDialog.classList.add('show');
+    // }
+});
+
+// closeButton.addEventListener('click', event => {
+//   shareDialog.classList.remove('show');
+// });
